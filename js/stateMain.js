f318d9eadbdbb312ae1fdfc2d824b9773accbc4d
Maureen Roble <moroble@gmail.com>
2017-01-11 15:23:36 -0500

    add sound folder

--- js/stateMain.js
+++ js/stateMain.js
@@ -2,17 +2,19 @@ var StateMain={
     
    preload:function()
     {
+    	//load images
        
     },
     
     create:function()
     {
+    	//objects, variables, functions
         
     },
     
     update:function()
     {       
-        
+        //constant running loop
     }    
     
 }
