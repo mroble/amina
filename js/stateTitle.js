f105be6b072d8dc133b76cd1efe2d7e51dff1313
Maureen Roble <moroble@gmail.com>
2017-01-11 16:01:27 -0500

    add phaser and stateTitle

---
+++ js/stateTitle.js
@@ -0,0 +1,18 @@
+var StateTitle={    
+    
+   preload:function()
+    {
+       game.load.image("logo","images/title/logo.png");
+    },
+    
+    create:function()
+    {
+        this.logo=game.add.sprite(0,0,"logo");
+    },
+    
+    update:function()
+    {       
+        
+    }    
+    
+}
