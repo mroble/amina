3fe8e367659af2d3319eb7480be38f69ef72d4b5
Maureen Roble <moroble@gmail.com>
2017-01-17 11:24:37 -0500

    add weaving folder to images

--- js/stateMain.js
+++ js/stateMain.js
@@ -2,23 +2,23 @@ var StateMain={
     
    preload:function()
     {
-    	game.load.sprite("blanktile", "images/tiles/aminaletter.png");
+    	game.load.image("blanktile", "images/tiles/aminaletter.png");
        
     },
     
     create:function()
     {
-    var blanktile = game.add.sprite(50,50, "blanktile");
-       var blanktile = game.add.sprite(50,62,"blanktile");
-        var blanktile = game.add.sprite(50,74,"blanktile");
+        var blanktile = game.add.sprite(0,0, "blanktile");
+        var blanktile1 = game.add.sprite(35,0,"blanktile");
+        var blanktile2 = game.add.sprite(120,0,"blanktile");
         
-        //this.tileGroup = game.add.group();
-        //this.tileGroup.add("blanktile");
-        //this.tileGroup.add("blanktile");
-        //this.tileGroup.add("blanktile");
+        this.tileGroup = game.add.group();
+        this.tileGroup.add("blanktile");
+        this.tileGroup.add("blanktile1");
+        this.tileGroup.add("blanktile2");
 
-        //this.tileGroup.x = game.world.centerX - this.tileGroup.width/2;
-        //this.tileGroup.y = game.height - 300;
+        //this.tileGroup.x = game.width/2;
+        this.tileGroup.y = game.height - 300;
 
 
     },
